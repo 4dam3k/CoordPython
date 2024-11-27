6a14e328a73d2de60efd0d29cdf01b470d3d61c8
4dam3k <66412368+4dam3k@users.noreply.github.com>
2024-11-27 22:31:05 +0100

    Update to Vertices of Polygon

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 axis_size = 16
 
-# ticks = np.arange(-axis_size, axis_size + 0.1, axis_size * 0.125)
-# ax.set_xticks(ticks)
-# ax.set_yticks(ticks)
-# ax.axis([-axis_size, axis_size] * 2)
-# ax.grid()
+ticks = np.arange(-axis_size, axis_size + 0.1, axis_size * 0.125)
+ax.set_xticks(ticks)
+ax.set_yticks(ticks)
+ax.axis([-axis_size, axis_size] * 2)
+ax.grid()
 
 rectangle_counter = 0
 
@@ -37,6 +37,8 @@ def PolyMaker(vertices, size):
         vertices.append(Point(vertices[0][0], vertices[0][1] - size[1]))  # BL
         return vertices
 
+    if len(vertices) == 3 or len(vertices) > 4 or len(vertices) == 0:
+        raise Exception("Invalid number of points provided")
 
 class Rectangle:
     def __init__(self, *Vertices, name=None, size=(0, 0), color=None, fill=False):
@@ -45,6 +47,9 @@ class Rectangle:
         if name is None:
             rectangle_counter += 1
             name = str(rectangle_counter)
+
+        # Make Vertices into Sympy Points
+        Vertices = [Point(v) for v in list(Vertices)]
 
         # Initialize the object properties
         self.Vertices = PolyMaker(list(Vertices), size)
@@ -58,17 +63,19 @@ class Rectangle:
             ax.plot(*x, "bo")
             ax.annotate(chr(ord('@') + i + 1) + self.name, (x[0] + 0.2, x[1] + 0.2))
 
-        polygon = plt.Polygon([(v.x, v.y) for v in self.Vertices],
+        polygon = plt.Polygon([v for v in self.Vertices],
                               fill=self.fill, color=self.color)
         ax.add_patch(polygon)
 
 
-list_of_polygons = []
+list_of_polygons = [Rectangle((x*6, 2), size=(2, 2), fill=False, color="orange") for x in range(0, 10)]
+for poly in list_of_polygons:
+    poly.Draw(ax)
 
-p1 = Rectangle(Point(0, 0), size=(4, 4), fill=False, color="orange")
-p1.Draw(ax)
-p2 = Rectangle(Point(-2, -4), size=(4,4), fill=False, color="purple")
-p2.Draw(ax)
+# p1 = Rectangle((0, 0), size=(4, 4), fill=False, color="orange")
+# p1.Draw(ax)
+# p2 = Rectangle((-2, -4), size=(4,4), fill=False, color="purple")
+# p2.Draw(ax)
 
 plt.show()
 
